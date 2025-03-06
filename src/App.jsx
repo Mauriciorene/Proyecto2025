47ba8df764c0b8aa919e0a049804ec33f314063f
Mauricio <rubiom831@gmail.com>
2025-03-06 09:10:46 -0800

    contenido en el centro de la pantalla.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,28 @@ import NumberChecker from "./NumberChecker";
 
 function App() {
   return (
-    <div>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        flexDirection: "column",
+        backgroundColor: "#f0f0f0",
+      }}
+    >
       <h1>Verificador de Números</h1>
-      <NumberChecker />
+      <div
+        style={{
+          background: "white",
+          padding: "20px",
+          borderRadius: "10px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+        }}
+      >
+        <NumberChecker />
+      </div>
     </div>
   );
 }
