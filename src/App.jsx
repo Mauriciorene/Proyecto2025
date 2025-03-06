808f5e6fa5292c42cec3ea97f6b16916ba1d41cc
Mauricio <rubiom831@gmail.com>
2025-03-06 09:15:15 -0800

    pantalla areglada.

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,25 @@ function App() {
   return (
     <div
       style={{
-        height: "100vh",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        height: "100vh",
+        width: "100vw", // El ancho de la pantalla
         textAlign: "center",
-        flexDirection: "column",
         backgroundColor: "#f0f0f0",
       }}
     >
-      <h1>Verificador de Números</h1>
       <div
         style={{
           background: "white",
           padding: "20px",
           borderRadius: "10px",
           boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+          minWidth: "300px", // Para que no se vea tan comprimido en móviles
         }}
       >
+        <h1>Verificador de Números</h1>
         <NumberChecker />
       </div>
     </div>
